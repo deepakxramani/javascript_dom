dcf25f6da40826fab436832c13e81fcb8d796fe9
Deepak Ramani <deepakramani65@gmail.com>
2024-10-23 13:43:22 +0530

    1 hour completed

--- script.js
+++ script.js
@@ -1,206 +1,310 @@
-    //* Selecting elements using DOM
-    
-    //& getElementById()
-    // let msg = document.getElementById("message")
-    // console.log(msg)
+//* Selecting elements using DOM
 
+//& getElementById()
+// let msg = document.getElementById("message")
+// console.log(msg)
 
-    //& getElementsByName()
-    // let btn = document.getElementsByName("language")
-    // console.log(btn)
+//& getElementsByName()
+// let btn = document.getElementsByName("language")
+// console.log(btn)
 
+//& getElementsByTagName()
+// let heading = document.getElementsByTagName("h1")
+// console.log(heading)
 
-    //& getElementsByTagName()
-    // let heading = document.getElementsByTagName("h1")
-    // console.log(heading)
+//& getElementsByClassName()
+// let msg = document.getElementsByClassName("message")
 
+// let cont = document.getElementById("container")
+// let msg = cont.getElementsByClassName("message")
+// console.log(msg)
 
-    //& getElementsByClassName()
-    // let msg = document.getElementsByClassName("message")
+//& querySelector() And querySelectorAll()
+//let msg = document.querySelector(".message")
+//console.log(msg);
 
-    // let cont = document.getElementById("container")
-    // let msg = cont.getElementsByClassName("message")
-    // console.log(msg)
+// let msg = document.querySelectorAll(".message")
+// console.log(msg);
 
+// Using elements:
+// let cont = document.getElementById("container")
+// let msg = cont.querySelector(".message")
+// let msg = cont.querySelectorAll(".message")
+// console.log(msg)
 
-    //& querySelector() And querySelectorAll()
-    //let msg = document.querySelector(".message")
-    //console.log(msg);
+// For accessing elements with ID:
+// let msg = document.querySelectorAll("#container")
+// let msg = document.querySelector("#container")
+// console.log(msg)
 
-    // let msg = document.querySelectorAll(".message")
-    // console.log(msg);
+// For accessing elements directly:
+// let msg = document.querySelectorAll("div")
+// let msg = document.querySelectorAll("div, h1")
+// console.log(msg)
 
-    // Using elements:
-    // let cont = document.getElementById("container")
-    // let msg = cont.querySelector(".message")
-    // let msg = cont.querySelectorAll(".message")
-    // console.log(msg)
+// * TRAVERSING ELEMENTS *
+// & To get the parent node of a specified node in the DOM tree, we can use the 'parentNode' property.
 
-    // For accessing elements with ID:
-    // let msg = document.querySelectorAll("#container")
-    // let msg = document.querySelector("#container")
-    // console.log(msg)
+// let txt = document.querySelector(".text")
+// console.log(txt.parentNode)
 
-    // For accessing elements directly:
-    // let msg = document.querySelectorAll("div")
-    // let msg = document.querySelectorAll("div, h1")
-    // console.log(msg)
+// * Selecting Child Elements *
+// & We will learn about how to get the 'firstChild' element, 'lastChild' element and 'all children' of the specified element.
 
+//let parent = document.querySelector(".title")
+//console.log(parent.firstChild)          // #text
 
+//console.log(parent.firstElementChild)  // <p>Welcome to GreatStack 1</p>
 
+//console.log(parent.lastElementChild)  // <p>Welcome to GreatStack 4</p>
 
-    // * TRAVERSING ELEMENTS *
-    // & To get the parent node of a specified node in the DOM tree, we can use the 'parentNode' property. 
+//console.log(parent.childNodes)  // NodeList(9) [text, p, ........4]
 
-    // let txt = document.querySelector(".text")
-    // console.log(txt.parentNode)
+// * Selecting Next Siblings and Previous Siblings of an element *
 
+//let second = document.querySelector(".second");
+//console.log(second.previousElementSibling)    //Previous Sibling of Second element -->> // <p>Welcome to GreatStack 1</p>
 
+//console.log(second.nextElementSibling)       //Next Sibling of Second element -->> // <p>Welcome to GreatStack 3</p>
 
-    // * Selecting Child Elements *
-    // & We will learn about how to get the 'firstChild' element, 'lastChild' element and 'all children' of the specified element.
+// * MANIPULATING HTML ELEMENTS *
+// * 1. createElement() method
 
-    //let parent = document.querySelector(".title")
-    //console.log(parent.firstChild)          // #text
+// let div = document.createElement("div");
+// div.innerHTML = "<p>Hello</p>"
+// console.log(div)
 
-    //console.log(parent.firstElementChild)  // <p>Welcome to GreatStack 1</p> 
+// // inserting newly created div element to the web page
+// document.body.appendChild(div)
 
-    //console.log(parent.lastElementChild)  // <p>Welcome to GreatStack 4</p> 
+// // adding Id's and ClassNames  to the newly created div element.
+// div.id = "title"
+// div.className = "title"
 
-    //console.log(parent.childNodes)  // NodeList(9) [text, p, ........4]
+// * 2. appendChild() method
 
+// let menu = document.getElementById("menu");
 
+// let list = document.createElement("li");
+// list.innerHTML = "Contact";
 
-     // * Selecting Next Siblings and Previous Siblings of an element *
+// menu.appendChild(list)
 
-     //let second = document.querySelector(".second");
-     //console.log(second.previousElementSibling)    //Previous Sibling of Second element -->> // <p>Welcome to GreatStack 1</p>
+// * textContent & innerText Property:
 
-     //console.log(second.nextElementSibling)       //Next Sibling of Second element -->> // <p>Welcome to GreatStack 3</p>
+//let menu = document.getElementById("menu");
 
+//console.log(menu.textContent)
 
+//console.log(menu.innerText)
 
+// Note:-> will show all the elements on the console even if
+// they are hidden using the css. Though they'll stay hidden
+// on the webpage.
 
-    // * MANIPULATING HTML ELEMENTS *
-    // * 1. createElement() method
+//menu.textContent = "Hello!"   // this will remove all the 'li' elements from the
+//  menu and add the 'Hello!' text on the webpage.
+//  But not on the console.
 
-    // let div = document.createElement("div");
-    // div.innerHTML = "<p>Hello</p>"
-    // console.log(div)
+// * innerHTML proprty:
 
-    // // inserting newly created div element to the web page
-    // document.body.appendChild(div)
+// let menu = document.getElementById("menu");
 
-    // // adding Id's and ClassNames  to the newly created div element.
-    // div.id = "title"
-    // div.className = "title"
+//menu.innerHTML = "Hello!";
+//menu.innerHTML = "<h1>Hello!</h1>";      // innerHTML is able to add text in tags.
+// innerHTML renders the HTML tags if found
 
+//menu.textContent = "<h1>Hello!</h1>";      // output on the webpage: <h1>Hello!</h1>
+// textContent does not render the HTML tags
+// they display them as it is.
 
-    
-    // * 2. appendChild() method
+// * after() Method:
 
-    // let menu = document.getElementById("menu");
+// we can use after() method to insert one or more
+// nodes after the element.
 
-    // let list = document.createElement("li");
-    // list.innerHTML = "Contact";
+// accepts multiple nodes/elements/strings -> we can insert multiple
+//  nodes/elements/strings to the parent node
 
-    // menu.appendChild(list)
+// like this -> menu.after(node1, node2, ... node N)
+// or
+// menu.after(str1, str2, ... str N)
 
+// let menu = document.getElementById("menu");
 
+// let newLi1 = document.createElement("li");
+// let newLi2 = document.createElement("li");
 
-    // * textContent & innerText Property:
+// newLi1.innerHTML = "Portfolio";
+// newLi2.innerHTML = "Us";
+// menu.after(newLi1, newLi2);
 
-    //let menu = document.getElementById("menu");
+// let str1 = "Zenny";
+// let str2 = "Goku";
+// menu.after(str1, str2);
 
-    //console.log(menu.textContent)
+//* append() Method:    --> similar to appendChild()
+// In summary, append() is generally more versatile than appendChild() because it
+// allows for adding multiple nodes and strings simultaneously, while appendChild()
+// is stricter and can only work with single nodes.
 
-    //console.log(menu.innerText)
+// In appendChild(): If you try to pass a string, it will throw an error.
 
-    // Note:-> will show all the elements on the console even if 
-   // they are hidden using the css. Though they'll stay hidden 
-   // on the webpage. 
+// const parent = document.getElementById("parent");
+// const child1 = document.createElement("div");
+// const child2 = "Hello World!";
+// parent.append(child1, child2);
 
-   //menu.textContent = "Hello!"   // this will remove all the 'li' elements from the 
-                                   //  menu and add the 'Hello!' text on the webpage.
-                                   //  But not on the console. 
-   
+//* prepend() method:
+// The prepend() method in JavaScript inserts content at the beginning of a
+// parent element. It allows adding multiple nodes or strings directly:
 
+// Key Features:
+// 1. Adds at the Start: Unlike append(), which adds to the end, prepend() inserts at the start.
+// 2. Multiple Arguments: Can insert multiple elements or text in one call.
+// 3. Text Support: Allows direct text insertion without creating a text node.
+// 4. No Return Value: Returns undefined.
 
-    // * innerHTML proprty:
+// Example:
+//const parent = document.getElementById("parent");
+//const child = document.createElement("li");
+//child.textContent = "Prepended";
+//parent.prepend(child, " - Text - ");
 
-    // let menu = document.getElementById("menu");
+//* insertAdjacentHTML() method:
+//* syntax: element.insertAdjacentHTML("positionName", "element")
 
-    //menu.innerHTML = "Hello!";          
-    //menu.innerHTML = "<h1>Hello!</h1>";      // innerHTML is able to add text in tags.
-                                            // innerHTML renders the HTML tags if found
+// let menu = document.getElementById("menu");
 
-    //menu.textContent = "<h1>Hello!</h1>";      // output on the webpage: <h1>Hello!</h1>
-                                              // textContent does not render the HTML tags
-                                              // they display them as it is.
+// menu.insertAdjacentHTML("afterbegin", "<li>Contact</li>" )
+// menu.insertAdjacentHTML("beforeend", "<li>Contact</li>" )
+// menu.insertAdjacentHTML("beforebegin", "<li>Contact</li>" )
+// menu.insertAdjacentHTML("afterend", "<li>Contact</li>" )
 
+//* replaceChild() method:
+//* syntax: element.replaceChild(newChild, oldChild)
 
-    // * after() Method:
+// use to replace old child with a new one!
 
-    // we can use after() method to insert one or more
-    // nodes after the element.
+// let menu = document.getElementById('menu');
+// let projectLi = document.getElementById('project');
 
-    // accepts multiple nodes/elements/strings -> we can insert multiple
-    //  nodes/elements/strings to the parent node
+// let li = document.createElement('li');
+// li.innerHTML = 'Soon';
 
-    // like this -> menu.after(node1, node2, ... node N)
-    // or
-    // menu.after(str1, str2, ... str N)
+// menu.replaceChild(li, projectLi);
 
-    // let menu = document.getElementById("menu");
-    
-    // let newLi1 = document.createElement("li");
-    // let newLi2 = document.createElement("li");
-    
-    // newLi1.innerHTML = "Portfolio";
-    // newLi2.innerHTML = "Us";
-    // menu.after(newLi1, newLi2);
+//* replaceChild() method:
+//* syntax: element.replaceChild(newChild, oldChild)
 
-    // let str1 = "Zenny";
-    // let str2 = "Goku";
-    // menu.after(str1, str2);
+// use to clone the old node!
 
+// let menu = document.getElementById('menu');
 
+// let newNode = menu.cloneNode(true);
+// let newNode = menu.cloneNode('menu');
+// console.log(newNode);
 
-    //* append() Method:    --> similar to appendChild()
-    // In summary, append() is generally more versatile than appendChild() because it 
-    // allows for adding multiple nodes and strings simultaneously, while appendChild() 
-    // is stricter and can only work with single nodes.
+// document.body.appendChild(newNode)
 
-    // In appendChild(): If you try to pass a string, it will throw an error.
+//* removeChild() method:
+//* Use to remove the child element of a node we can use removeChild() method
 
-    // const parent = document.getElementById("parent");
-    // const child1 = document.createElement("div");
-    // const child2 = "Hello World!";
-    // parent.append(child1, child2);
+//* syntax: parentNode.removeChild(childNode);
 
+//let menu = document.getElementById('menu');
 
+//menu.removeChild(menu.lastElementChild);    // will remove the 'project' from the list
+//menu.removeChild(menu.firstElementChild);    // will remove the 'Home' from the list
 
+//* insertBefore() method:
+//* Use insertBefore() method to insert a new node before an exisiting node as a child node a parent node.
 
-    //* prepend() method:
-    // The prepend() method in JavaScript inserts content at the beginning of a 
-    // parent element. It allows adding multiple nodes or strings directly:
+//* syntax: parentNode.insertBefore(newNode, existingNode);
 
-    // Key Features:
-    // 1. Adds at the Start: Unlike append(), which adds to the end, prepend() inserts at the start.
-    // 2. Multiple Arguments: Can insert multiple elements or text in one call.
-    // 3. Text Support: Allows direct text insertion without creating a text node.
-    // 4. No Return Value: Returns undefined.
+// let menu = document.getElementById('menu');
+// let li = document.createElement("li");
+// li.innerHTML = "Hackathon"
+// menu.insertBefore(li, menu.firstElementChild)
 
-    // Example: 
-    //const parent = document.getElementById("parent");
-    //const child = document.createElement("li");
-    //child.textContent = "Prepended";
-    //parent.prepend(child, " - Text - ");
+//* ATTRIBUTES METHODS
 
+// let inputBox = document.getElementById("username")
+// console.log(inputBox.attributes)
 
+//* 1. getAttribute() method
 
-    
+//console.log(inputBox.getAttribute("placeholder"))
+//console.log(inputBox.getAttribute("type"))
 
+//* 2. setAttribute() method
+
+// let inputBox = document.getElementById('username');
+
+// inputBox.setAttribute('name', 'username');
+// inputBox.setAttribute('class', 'user');
+
+// console.log(inputBox.attributes)
+// console.log(inputBox.getAttribute('name'))
+// console.log(inputBox.getAttribute('class'))
+// console.log(inputBox)
+
+//* 3. hasAttribute() method
+
+// let inputBox = document.getElementById('username');
+
+// console.log(inputBox.hasAttribute("class"))
+// console.log(inputBox.hasAttribute("id"))
+
+//* 4. removeAttribute() method
+// That will remove the attribute from the element
+
+// let inputBox = document.getElementById('username');
+
+// console.log(inputBox.attributes);
+// inputBox.removeAttribute("placeholder")
+// console.log(inputBox.attributes)
+
+//* MANIPULATINGS ELEMENT'S STYLE:
+
+// let inputBox = document.getElementById('username');
+
+// to get all the styles
+// console.log(inputBox.style);
+
+// to get the specific style
+// console.log(inputBox.style.backgroundColor);  // black
+// console.log(inputBox.style.fontSize);        //  20px
+
+
+
+//* setting styles to the elements:
+
+
+// let inputBox = document.getElementById('username');
+
+// inputBox.style.padding = "20px"
+
+// console.log(inputBox)
+
+
+//* another way to add inline css using "cssText" property...
+
+let inputBox = document.getElementById('username');
+
+// But this will overwrite the existing css
+//inputBox.style.cssText = "width: 200px"
+//console.log(inputBox)
+
+// If you want to keep the all the inline css property then use concatenating "+="
+// inputBox.style.cssText += "width: 200px"
+// console.log(inputBox)
+
+
+// we can add multiple styles using this cssText property
+inputBox.style.cssText = "width: 200px; height: 100px"
+console.log(inputBox.style)
+console.log(inputBox.style.getPropertyValue('height'))
 
 
 
